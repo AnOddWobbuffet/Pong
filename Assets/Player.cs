8cba339ee64fab16780bd32481ad6f99f17b3e88
Andy Chen <andy_c120@hotmail.com>
2021-05-12 00:00:38 -0400

    project

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public bool isLeftSide;
+    public float speed;
+    public Rigidbody2D rigidBody;
+
+
+    [SerializeField] private GameObject hehe;
+    private float movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLeftSide)
+        {
+            movement = Input.GetAxisRaw("VerticalLeft");
+        }    
+        else
+        {
+            movement = Input.GetAxisRaw("VerticalRight");
+        }
+
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, movement * speed);
+
+    }
+    
+}
